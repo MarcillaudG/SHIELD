5d3762b0a14aa83d0bef07c7590e9dd097cd9708
augustin.degas <augustin.degas1@gmail.com>
2020-10-28 14:24:54 +0100

    Add Variable tools

--- SUGAR/src/fr/irit/smac/shield/model/VariableState.java
+++ SUGAR/src/fr/irit/smac/shield/model/VariableState.java
@@ -1,6 +1,7 @@
 package fr.irit.smac.shield.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class VariableState implements Serializable {
 	protected String name;
@@ -31,4 +32,25 @@ public class VariableState implements Serializable {
 	public String toString() {
 		return "[name=" + name + ", value=" + value+"]";
 	}
+	
+	
+	public static int numberVariablesActive(List<VariableState> variableStateList) {
+		int counter = 0;
+		
+		for(VariableState var: variableStateList) {
+			if(var.getValue() != 0)
+				counter++;
+		}
+		return counter;
+	}
+	
+	public static VariableState getState(List<VariableState> variableStateList,  String idVariable) {
+		for (VariableState variableState : variableStateList) {
+			if(variableState.getName().equals(idVariable)) {
+				return variableState;
+			}
+		}
+		return null;
+	}
+	
 }
